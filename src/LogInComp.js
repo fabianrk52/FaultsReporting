4390377bbf6d41992694ff95734b7a28a0c2399c
elephantgitdev <elephantgit@gmail.com>
2020-03-31 21:31:38 +0300

    Create LogInComp.js
    Preparation for registration or login component

---
+++ src/LogInComp.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcom to React</h1>
+        </header>
+        <p className="App-intro">
+          To Get started, edit <code> and save to reload.</code>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
